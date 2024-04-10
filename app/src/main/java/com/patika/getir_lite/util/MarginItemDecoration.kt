8a96b29bff6aa50355044497ab98eff6a20e754f
f-arslan <buildsmstff@gmail.com>
2024-04-11 00:35:00 +0300

    feat: custom decor for recycle view

---
+++ app/src/main/java/com/patika/getir_lite/util/MarginItemDecoration.kt
@@ -0,0 +1,27 @@
+package com.patika.getir_lite.util
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.patika.getir_lite.R
+
+class MarginItemDecoration : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view)
+        val itemCount = state.itemCount
+        val margin = view.context.resources.getDimensionPixelSize(R.dimen.margin_start_end)
+
+        if (position == 0) {
+            outRect.left = margin
+        }
+
+        if (position == itemCount - 1) {
+            outRect.right = margin
+        }
+    }
+}
