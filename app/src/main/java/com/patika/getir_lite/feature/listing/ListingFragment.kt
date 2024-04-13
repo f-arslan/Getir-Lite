b516e4e5282c06f6c86d3fbb04fc7a7b4748e87f
f-arslan <buildsmstff@gmail.com>
2024-04-13 18:34:48 +0300

    feat: grid layout impl

--- app/src/main/java/com/patika/getir_lite/feature/listing/ListingFragment.kt
+++ app/src/main/java/com/patika/getir_lite/feature/listing/ListingFragment.kt
@@ -1,14 +1,17 @@
 package com.patika.getir_lite.feature.listing
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import com.patika.getir_lite.ProductViewModel
 import com.patika.getir_lite.databinding.FragmentListingBinding
 import com.patika.getir_lite.feature.BaseFragment
-import com.patika.getir_lite.feature.listing.adapter.SuggestedProductAdapter
+import com.patika.getir_lite.feature.listing.adapter.ProductAdapter
 import com.patika.getir_lite.model.Response
+import com.patika.getir_lite.util.GridSpacingItemDecoration
 import com.patika.getir_lite.util.MarginItemDecoration
 import com.patika.getir_lite.util.ext.scopeWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,8 +23,10 @@ import kotlinx.coroutines.flow.map
 @AndroidEntryPoint
 class ListingFragment : BaseFragment<FragmentListingBinding>() {
 
+    private val productViewModel: ProductViewModel by activityViewModels()
     private val viewModel: ListingViewModel by viewModels()
-    private lateinit var suggestedAdapter: SuggestedProductAdapter
+    private lateinit var suggestedProductAdapter: ProductAdapter
+    private lateinit var productAdapter: ProductAdapter
 
     override fun inflateBinding(
         inflater: LayoutInflater,
@@ -30,38 +35,53 @@ class ListingFragment : BaseFragment<FragmentListingBinding>() {
         FragmentListingBinding.inflate(inflater, container, false)
 
     override fun FragmentListingBinding.initializeViews() {
-        viewModel.fetchProductData()
         setupAdapter()
         observeRemoteChanges()
     }
 
     private fun FragmentListingBinding.setupAdapter() {
-        suggestedAdapter = SuggestedProductAdapter()
-        rvSuggested.adapter = suggestedAdapter
-        rvSuggested.addItemDecoration(MarginItemDecoration())
+        suggestedProductAdapter = ProductAdapter()
+        rvSuggestedProduct.adapter = suggestedProductAdapter
+        rvSuggestedProduct.addItemDecoration(MarginItemDecoration())
+
+        productAdapter = ProductAdapter()
+        rvProduct.layoutManager = GridLayoutManager(requireContext(), 3)
+        rvProduct.addItemDecoration(GridSpacingItemDecoration())
+        rvProduct.adapter = productAdapter
     }
 
     private fun FragmentListingBinding.observeRemoteChanges() {
         scopeWithLifecycle {
-            viewModel.remoteUiState.map { it.product }.distinctUntilChanged().collectLatest {
-            }
+            productViewModel.remoteUiState.map { it.product }.distinctUntilChanged()
+                .collectLatest { response ->
+                    when (response) {
+                        is Response.Error -> {}
+                        Response.Loading -> {}
+
+                        is Response.Success -> {
+                            productAdapter.saveData(response.data)
+                        }
+                    }
+                }
         }
+
         scopeWithLifecycle {
-            viewModel.remoteUiState.map { it.suggestedProduct }.distinctUntilChanged()
+            productViewModel.remoteUiState.map { it.suggestedProduct }.distinctUntilChanged()
                 .collectLatest { response ->
                     when (response) {
                         is Response.Error -> {}
                         Response.Loading -> {
                             shimmerLayout.startShimmer()
                         }
+
                         is Response.Success -> {
-                            suggestedAdapter.saveData(response.data)
-                            delay(4000L)
-                            with(shimmerLayout) {
+                            delay(5000L)
+                            suggestedProductAdapter.saveData(response.data)
+                            shimmerLayout.run {
                                 stopShimmer()
                                 visibility = View.GONE
                             }
-                            rvSuggested.visibility = View.VISIBLE
+                            rvSuggestedProduct.visibility = View.VISIBLE
                         }
                     }
                 }
