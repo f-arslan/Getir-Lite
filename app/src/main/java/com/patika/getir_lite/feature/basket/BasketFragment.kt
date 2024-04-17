b3806f8630a45fbed86f1f7c5d0ef61ef436745b
f-arslan <buildsmstff@gmail.com>
2024-04-17 12:55:19 +0300

    feat: implementing both list in basket fragment

--- app/src/main/java/com/patika/getir_lite/feature/basket/BasketFragment.kt
+++ app/src/main/java/com/patika/getir_lite/feature/basket/BasketFragment.kt
@@ -4,10 +4,23 @@ import android.text.SpannableString
 import android.text.style.StrikethroughSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ConcatAdapter
+import com.patika.getir_lite.ProductViewModel
 import com.patika.getir_lite.R
+import com.patika.getir_lite.data.local.model.toDomainModel
 import com.patika.getir_lite.databinding.FragmentBasketBinding
+import com.patika.getir_lite.databinding.ItemBasketBinding
 import com.patika.getir_lite.feature.BaseFragment
+import com.patika.getir_lite.feature.ProductAdapter
+import com.patika.getir_lite.feature.basket.adapter.HeaderAdapter
+import com.patika.getir_lite.feature.basket.adapter.ProductListAdapter
+import com.patika.getir_lite.model.BaseResponse
+import com.patika.getir_lite.util.decor.DividerDecoration
+import com.patika.getir_lite.util.ext.formatPrice
 import com.patika.getir_lite.util.ext.scopeWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -16,6 +29,9 @@ import kotlinx.coroutines.flow.collectLatest
 class BasketFragment : BaseFragment<FragmentBasketBinding>() {
 
     private val viewModel: BasketViewModel by viewModels()
+    private val productViewModel: ProductViewModel by activityViewModels()
+    private lateinit var basketProductAdapter: ProductAdapter<ItemBasketBinding>
+    private lateinit var productListAdapter: ProductListAdapter
 
     override fun inflateBinding(
         inflater: LayoutInflater,
@@ -23,26 +39,78 @@ class BasketFragment : BaseFragment<FragmentBasketBinding>() {
     ): FragmentBasketBinding = FragmentBasketBinding.inflate(inflater, container, false)
 
     override fun FragmentBasketBinding.onMain() {
-        setupTextViews()
+        setupTextView()
+        setupRecycleViewAndAdapters()
         listenBasketWithProducts()
+        listenSuggestedProducts()
     }
 
-    private fun listenBasketWithProducts() = with(viewModel) {
+    private fun FragmentBasketBinding.setupRecycleViewAndAdapters() {
+        basketProductAdapter = ProductAdapter(
+            bindingInflater = ItemBasketBinding::inflate,
+            events = productViewModel::onEvent,
+        )
+        val divider = ContextCompat.getDrawable(requireContext(), R.drawable.divider)
+        val margin = resources.getDimensionPixelSize(R.dimen.margin_8)
+        divider?.let {
+            rvBasket.addItemDecoration(DividerDecoration(divider, margin))
+        }
+
+        val headerAdapter = HeaderAdapter(getString(R.string.suggested_product))
+        productListAdapter = ProductListAdapter(
+            events = productViewModel::onEvent,
+            onProductClick = ::navigateToDetailFragment
+        )
+
+        val concatAdapter = ConcatAdapter(basketProductAdapter, headerAdapter, productListAdapter)
+        rvBasket.adapter = concatAdapter
+    }
+
+    private fun navigateToDetailFragment(productId: Long) {
+        if (isAdded) {
+            findNavController().navigate(
+                BasketFragmentDirections.actionBasketFragmentToDetailFragment(productId)
+            )
+        }
+    }
+
+    private fun FragmentBasketBinding.listenSuggestedProducts() = with(productViewModel) {
         scopeWithLifecycle {
-            basketWithProducts.collectLatest {
-                println(it)
+            suggestedProducts.collectLatest { response ->
+                when (response) {
+                    is BaseResponse.Error -> Unit
+                    BaseResponse.Loading -> Unit
+
+                    is BaseResponse.Success -> {
+                        productListAdapter.saveData(response.data)
+                    }
+                }
             }
         }
     }
 
-    private fun FragmentBasketBinding.setupTextViews() = with(tvOldPrice) {
-        layoutToolbar.textView.text = getString(R.string.basket_title)
-        tvFinalPrice.text = getString(R.string._2000_00)
+    private fun FragmentBasketBinding.listenBasketWithProducts() = with(viewModel) {
+        scopeWithLifecycle {
+            basketWithProducts.collectLatest { response ->
+                when (response) {
+                    is BaseResponse.Success -> {
+                        val data = response.data
+                        val price = data.order.totalPrice.formatPrice()
+                        basketProductAdapter.saveData(data.products.map { it.toDomainModel() })
+                        tvFinalPrice.text = price
+                        SpannableString(price).apply {
+                            setSpan(StrikethroughSpan(), 0, price.length, 0)
+                            tvOldPrice.text = this
+                        }
+                    }
 
-        text = getString(R.string._2000_00) // Debug purposes
-        SpannableString(text).apply {
-            setSpan(StrikethroughSpan(), 0, text.length, 0)
-            text = this
+                    else -> Unit
+                }
+            }
         }
+    }
+
+    private fun FragmentBasketBinding.setupTextView() {
+        layoutToolbar.textView.text = getString(R.string.basket_title)
     }
 }
