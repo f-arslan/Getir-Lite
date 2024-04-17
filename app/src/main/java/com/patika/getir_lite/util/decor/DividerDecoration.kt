f3673579f86c8284ce954e9658fd5135d2260f6c
f-arslan <buildsmstff@gmail.com>
2024-04-17 12:54:48 +0300

    fix: transition overlap fix

--- app/src/main/java/com/patika/getir_lite/util/decor/DividerDecoration.kt
+++ app/src/main/java/com/patika/getir_lite/util/decor/DividerDecoration.kt
@@ -4,15 +4,22 @@ import android.graphics.Canvas
 import android.graphics.drawable.Drawable
 import androidx.recyclerview.widget.RecyclerView
 
-class DividerDecoration(private val divider: Drawable) : RecyclerView.ItemDecoration() {
+class DividerDecoration(private val divider: Drawable, private val margin: Int) :
+    RecyclerView.ItemDecoration() {
     override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        val left = parent.paddingLeft
-        val right = parent.width - parent.paddingRight
+        val left = parent.paddingLeft + margin
+        val right = parent.width - parent.paddingRight - margin
+
         val childCount = parent.childCount
         for (i in 0 until childCount) {
             val child = parent.getChildAt(i)
-            val params = child.layoutParams as RecyclerView.LayoutParams
 
+            // Skip drawing for children in transition
+            if (child.translationX != 0f || child.translationY != 0f || child.alpha != 1f) {
+                continue
+            }
+
+            val params = child.layoutParams as RecyclerView.LayoutParams
             val top = child.bottom + params.bottomMargin
             val bottom = top + divider.intrinsicHeight
 
